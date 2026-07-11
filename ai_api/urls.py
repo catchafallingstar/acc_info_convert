4744d9bed5f2139bb16a62aa1c3cf4b890b82105
catchafallingstar <bxybai@umich.edu>
2026-07-10 23:24:34 -0400

    solve api/urls.py urlpattern duplicated api words

--- ai_api/urls.py
+++ ai_api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+   # Resolves to /api/process-ai/
     path('process-ai/', views.process_ai, name='process_ai'),
-    path('api/generate-pdf/', views.generate_accessible_pdf, name='generate_pdf'),
+    
+    # Resolves to /api/generate-pdf/
+    path('generate-pdf/', views.generate_accessible_pdf, name='generate_pdf'),
 ]
