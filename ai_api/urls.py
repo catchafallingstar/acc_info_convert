06e15592926e81472ba7d49e25257261bfba91ce
catchafallingstar <bxybai@umich.edu>
2026-07-13 00:51:51 -0400

    fix pdf img

--- ai_api/urls.py
+++ ai_api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
    # Resolves to /api/process-ai/
     path('process-ai/', views.process_ai, name='process_ai'),
     
-    # Resolves to /api/generate-pdf/
-    path('generate-pdf/', views.generate_accessible_pdf, name='generate_pdf'),
+    # Resolves to /api/generate-accessible-pdf/
+    path('generate-accessible-pdf/', views.generate_accessible_pdf, name='generate_accessible_pdf'),
     path('process-pdf-ai/', views.process_pdf_ai, name='process_pdf_ai'),
 ]
